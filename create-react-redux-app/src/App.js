a3b5b5b1e4059c75343d9db4ee2f1ef4e704a335
Ariane Gomes de Sousa <ariane.sousa011@gmail.com>
2020-02-29 16:28:09 -0300

    utilizando redux

--- create-react-redux-app/src/App.js
+++ create-react-redux-app/src/App.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 
 
 class App extends Component {
+
+  state = {
+    inputValue: 'botao'
+  }
+
+  inputChange = event => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
   render() {
-    const { newValue } = this.props;
+    const { 
+      newButton, // maDispathToProps
+      newValue // mapStateToProps
+    } = this.props;
     return (
       <div className="App" style={{ paddingTop: '10px' }}>
-        <input type='text' />
-        <button>
+        <input 
+        onChange={this.inputChange}
+        type='text'
+        value={this.state.inputValue} />
+        <button onClick={() => clickButton(this.state.inputValue)}>
           Clique me!
         </button>
         <h1>{newValue}</h1>
