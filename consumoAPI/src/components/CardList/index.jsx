e2d5b68ecb551ceb0b6087f34dcbc8818b031f3f
Ariane Gomes de Sousa <ariane.sousa011@gmail.com>
2020-03-22 21:37:33 -0300

    ajuste de indentação

--- consumoAPI/src/components/CardList/index.jsx
+++ consumoAPI/src/components/CardList/index.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Cards from '../Cards'
 
-const  CardList = () => {
-    const [movies, setMovies] = useState([]) 
+    const CardList = () => {
+        const [movies, setMovies] = useState([])
 
-    useEffect(() => {
-        fetch('https://ghibliapi.herokuapp.com/films')
-            .then(response => response.json())
+        useEffect(() => {
+            fetch('https://ghibliapi.herokuapp.com/films')
+                .then(response => response.json())
                 .then(data => setMovies(data))
-        .catch(err =>  {
-            console.log(err);
-        })
-    }, [movies])
-    return (
-        <div className="container">
-            {
-                movies.map(movie => {
-                    return (
-                        <Cards key={movie.id} title={movie.title} description={movie.description}></Cards>
-                    )
+                .catch(err => {
+                    console.log(err);
                 })
-            }
-        </div>
-    )
-}
+        }, [movies])
+        return (
+            <div className="container">
+                {
+                    movies.map(movie => {
+                        return (
+                            <Cards key={movie.id} title={movie.title} description={movie.description}></Cards>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
 
 export default CardList;
-    
+
